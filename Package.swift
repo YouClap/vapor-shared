54369dceb9ac8a4a039f59a06e86be91c40460cf
portellaa <lcportellaa@gmail.com>
2019-08-27 11:03:18 +0100

    Rename target package

--- Package.swift
+++ Package.swift
@@ -7,13 +7,13 @@ let package = Package(
     name: "YouClap",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "YouClap", targets: ["youclap"]),
+        .library(name: "YouClap", targets: ["YouClap"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
         .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.1"),
     ],
     targets: [
-        .target(name: "youclap", dependencies: ["Vapor", "FluentMySQL"], path: "Sources"),
+        .target(name: "YouClap", dependencies: ["Vapor", "FluentMySQL"]),
     ]
 )
