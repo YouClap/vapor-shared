63d56b740268ccb2e48e012678c02d5a5c6e3b71
portellaa <lcportellaa@gmail.com>
2019-10-17 13:43:18 +0100

    feat(BACKEND-226): Generate FirebaseID

---
+++ Sources/YouClap/Firebase.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public enum Firebase {
+    private static let identifierLength = 20
+    private static let identifierAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    private static var randomGenerator = SystemRandomNumberGenerator()
+
+    public var autoIdentifier: String {
+        return String((0..<Self.identifierLength).compactMap { _ -> Character? in
+            return Self.identifierAlphabet.randomElement(using: &Self.randomGenerator)
+        })
+    }
+}
