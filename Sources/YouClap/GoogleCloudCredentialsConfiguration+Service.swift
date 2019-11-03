60c8491f2a1a013861f4a7f2420f9d40335dd2f0
portellaa <lcportellaa@gmail.com>
2019-11-03 10:29:50 +0000

    fix: Add GoogleCloud conformance to Vapor Service

---
+++ Sources/YouClap/GoogleCloudCredentialsConfiguration+Service.swift
@@ -0,0 +1,4 @@
+import Foundation
+import GoogleCloudKit
+
+extension GoogleCloudCredentialsConfiguration: Service {}
---
+++ Sources/YouClap/PubSubConfiguration+Service.swift
@@ -0,0 +1,4 @@
+import Foundation
+import GoogleCloudPubSubKit
+
+extension PubSubConfiguration: Service {}
