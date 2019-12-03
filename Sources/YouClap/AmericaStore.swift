f47c06e0e8b12bbcd0c7fe6f4038da0e19e8f275
portellaa <lcportellaa@gmail.com>
2019-12-03 11:52:15 +0000

    perf: Improve AmericaMessage for passed to store

--- Sources/YouClap/AmericaStore.swift
+++ Sources/YouClap/AmericaStore.swift
@@ -1,42 +1,17 @@
 import Foundation
 import GoogleCloudPubSubKit
+import NIO
 
 public protocol AmericaStore: Service {
-    func create(documents: AmericaDocumentInfo...) -> EventLoopFuture<PublishResponse>
-    func update(documents: AmericaDocumentInfo...) -> EventLoopFuture<PublishResponse>
-    func markDeleted(collection: AmericaMessage.Collection, documentID: String) -> EventLoopFuture<PublishResponse>
-    func delete(collection: AmericaMessage.Collection, documentID: String) -> EventLoopFuture<PublishResponse>
+    func publish(messages: AmericaMessage...) -> EventLoopFuture<PublishResponse>
 }
 
 extension Store: AmericaStore {
     private var topic: ProjectTopic { ProjectTopic(project: messageQueueClient.project, name: "america") }
 
-    public func create(documents: AmericaDocumentInfo...) -> EventLoopFuture<PublishResponse> {
-        let americaMessages = documents.map {
-            AmericaMessage(action: .create, collection: $0.collection, documentID: $0.documentID, model: $0.model)
-        }
-
-        return messageQueueClient.publish(messages: americaMessages, to: topic)
-    }
-
-    public func update(documents: AmericaDocumentInfo...) -> EventLoopFuture<PublishResponse> {
-        let americaMessages = documents.map {
-            AmericaMessage(action: .update, collection: $0.collection, documentID: $0.documentID, model: $0.model)
-        }
-
-        return messageQueueClient.publish(messages: americaMessages, to: topic)
-    }
-
-    public func markDeleted(collection: AmericaMessage.Collection, documentID: String) -> EventLoopFuture<PublishResponse> {
-        let americaMessage = AmericaMessage(action: .markDeleted, collection: collection, documentID: documentID)
-
-        return messageQueueClient.publish(messages: americaMessage, to: topic)
-    }
-
-    public func delete(collection: AmericaMessage.Collection, documentID: String) -> EventLoopFuture<PublishResponse> {
-        let americaMessage = AmericaMessage(action: .delete, collection: collection, documentID: documentID)
-
-        return messageQueueClient.publish(messages: americaMessage, to: topic)
+    public func publish(messages: AmericaMessage...) -> EventLoopFuture<PublishResponse> {
+        return messageQueueClient.publish(messages: messages, to: topic)
     }
 }
+
 
