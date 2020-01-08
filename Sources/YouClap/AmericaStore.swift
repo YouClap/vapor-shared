bbbc6760df52e0dadf50235fa31d4bb42e72b784
Luís Portela Afonso <luis.portela@youclap.tech>
2020-01-08 11:41:27 +0000

    fix: Remove AmericaStore conformance with Store (#9)

--- Sources/YouClap/AmericaStore.swift
+++ Sources/YouClap/AmericaStore.swift
@@ -3,13 +3,20 @@ import GoogleCloudPubSubKit
 import NIO
 
 public protocol AmericaStore: Service {
+    var messageQueueClient: PublisherClient { get }
+
     func publish(messages: AmericaMessage...) -> EventLoopFuture<PublishResponse>
+    func publish(messages: [AmericaMessage]) -> EventLoopFuture<PublishResponse>
 }
 
-extension Store: AmericaStore {
+extension AmericaStore {
     private var topic: ProjectTopic { ProjectTopic(project: messageQueueClient.project, name: "america") }
 
     public func publish(messages: AmericaMessage...) -> EventLoopFuture<PublishResponse> {
+        return messageQueueClient.publish(messages: messages, to: topic)
+    }
+
+    public func publish(messages: [AmericaMessage]) -> EventLoopFuture<PublishResponse> {
         return messageQueueClient.publish(messages: messages, to: topic)
     }
 }
