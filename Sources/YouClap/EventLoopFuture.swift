7ec59c983b0bb046d519de2b5a1e0e323c0f0c04
Luís Portela Afonso <luis.portela@youclap.tech>
2020-01-17 12:37:05 +0000

    feat: Support multiple eventloop from flatMap (#10)

--- Sources/YouClap/EventLoopFuture.swift
+++ Sources/YouClap/EventLoopFuture.swift
@@ -2,8 +2,26 @@ import NIO
 
 public extension EventLoopFuture {
     func mapError(_ callback: @escaping (Error) -> Error) -> EventLoopFuture<T> {
-        return thenIfError { error -> EventLoopFuture<T> in
-            self.eventLoop.newFailedFuture(error: callback(error))
+        thenIfError(self.eventLoop.newFailedFuture)
+    }
+
+    func flatMap<U>(_ callback: @escaping (T) throws -> [EventLoopFuture<U>]) -> EventLoopFuture<[U]> {
+        then {
+            do {
+                return EventLoopFuture<U>.whenAll(try callback($0), eventLoop: self.eventLoop)
+            } catch {
+                return self.eventLoop.newFailedFuture(error: error)
+            }
+        }
+    }
+
+    func flatMap<U>(_ callback: @escaping (T) throws -> [EventLoopFuture<U>]) -> EventLoopFuture<Void> {
+        then {
+            do {
+                return EventLoopFuture<U>.andAll(try callback($0), eventLoop: self.eventLoop)
+            } catch {
+                return self.eventLoop.newFailedFuture(error: error)
+            }
         }
     }
 }
